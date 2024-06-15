0ce1b92c8f4ca865a6f085b1a6682671aaadb758
sndp231998 <sandipchapagain797@gmail.com>
2024-06-15 14:45:00 +0545

    initial

---
+++ src/main/java/com/user/UserRegReadyApplication.java
@@ -0,0 +1,13 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegReadyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/user/entities/User.java
@@ -0,0 +1,16 @@
+package com.user.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+}
